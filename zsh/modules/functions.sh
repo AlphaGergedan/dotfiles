0f64cc5df17d7b50018423bdda40fa32bdd4a18e
Atamert Rahma <atamertiel@gmail.com>
2024-06-09 23:21:34 +0200

    add new function: zshmod
    A minimalistic function to handle zsh modules. Similar to modules system
    in clusters, it can list, load and unload modules instead of sourcing
    them all at once at shell start!

--- zsh/modules/functions.sh
+++ zsh/modules/functions.sh
@@ -173,11 +173,74 @@ function print_separator() {
   printf -- "%s\n" "${separator:$str_pos+$str_len}"
 }
 
-# lists
 function ls-zsh-modules() {
   print_separator "Available ZSH Modules"
   echo "$zsh_modules_ls_"
   print_separator
+}
+
+function zshmod() {
+  local usage="Usage: zshmod {list|load|unload} [module-name]"
+
+  if [[ $# -eq 0 ]]; then
+    echo $usage
+    return 1
+  fi
+
+  local command=$1
+  case "$command" in
+    list)
+      print_separator "Unloaded ZSH Modules"
+      echo "\n$unloaded_zsh_modules_ls_\n"
+      print_separator "Loaded ZSH Modules"
+      echo "\n$loaded_zsh_modules_ls_\n"
+      print_separator
+      ;;
+    load)
+      if [[ -z "$2" ]]; then
+        echo "Module name missing"
+        echo $usage
+        return 1
+      fi
+
+      if [[ ${loaded_zsh_modules_ls_[@]} =~ "$2" ]]; then
+        echo "Module $2 already loaded"
+        return 0
+      fi
+
+      if [[ ! ${zsh_modules_ls_[@]} =~ ${2} ]]; then
+        echo "Module $2 not found"
+        return 1
+      fi
+
+      source $2
+      loaded_zsh_modules_ls_+=($2)
+
+      local tmp=()
+      # remove the element from unloaded list
+      for mod_ in "${unloaded_zsh_modules_ls_[@]}"; do
+        if [[ "$mod_" != "$2" ]]; then
+          tmp+=("$mod_")
+        fi
+      done
+
+      unloaded_zsh_modules_ls_=$tmp
+
+      echo "Module $2 loaded"
+      return 0
+      ;;
+    unload)
+      # TODO: unload the given module and fix the loaded unloaded modules list
+      echo "Not implemented yet."
+      return 1
+      ;;
+    *)
+      echo "Unknown command: $command"
+      echo $usage
+      return 1
+      ;;
+
+  esac
 }
 
 function confirm_cmd() {
--- zshrc
+++ zshrc
@@ -4,6 +4,7 @@
 #
 #########################################
 
+# FIXME: automize default modules, put them in a dir
 ZSH_MODULES_="$HOME/.zsh/modules"
 
 env_vars_="$ZSH_MODULES_/env-vars.sh"
@@ -22,16 +23,20 @@ history_substring_search_="$ZSH_MODULES_/history-substring-search.sh"
 
 startup_="$ZSH_MODULES_/startup.sh"
 
-# all modules
-zsh_modules_ls_=$(ls $ZSH_MODULES_)
+zsh_modules_ls_=($(find $ZSH_MODULES_ -type f))
 
 # default modules to source at startup
-default_zsh_modules_ls_=($env_vars_ $options_ $aliases_ $functions_ $keybindings_ $pure_ $nvim_ $completion_ $syntax_highlighting_ $history_substring_search_)
+loaded_zsh_modules_ls_=($env_vars_ $options_ $aliases_ $functions_ $keybindings_ $pure_ $nvim_ $completion_ $syntax_highlighting_ $history_substring_search_ $startup_)
 
 # source default modules
-for mod_ in $default_zsh_modules_ls_; do
+for mod_ in $loaded_zsh_modules_ls_; do
   test -e "$mod_" && source "$mod_"
 done
 
-# STARTUP CALLS
-test -e "$startup_" && source "$startup_"
+# set unloaded modules list
+unloaded_zsh_modules_ls_=$()
+for mod_ in "${zsh_modules_ls_[@]}"; do
+  if [[ ! ${loaded_zsh_modules_ls_[@]} =~ "$mod_" ]]; then
+    unloaded_zsh_modules_ls_+=($mod_)
+  fi
+done
