38dcb239b89fa406d9ec2f0500c3b200ebf1611d
Atamert Rahma <atamertiel@gmail.com>
2024-06-27 21:54:20 +0200

    add home local bin to path

--- zsh/modules/startup.sh
+++ zsh/modules/startup.sh
@@ -11,4 +11,7 @@ xset r rate 150 50
 #eval "xinput set-button-map $(xinput list | grep Touchpad | sed -e 's/^.*id=//' -e 's/\s.*$//') 1 2 3 5 4 6 7"
 #eval "xinput set-button-map $(xinput list | grep SteelSeries | sed -e 's/^.*id=//' -e 's/\s.*$//') 1 2 3 5 4 6 7"
 
+# see user packages first
+path_prepend $HOME/.local/bin
+
 echo "-> $(basename $0 .sh) loaded.."
