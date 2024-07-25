fc4786d5d94b4b1b9457236615acfa120f5e7933
Atamert Rahma <atamertrahma@gmail.com>
2024-07-25 16:01:02 +0200

    source rvm on ruby mod

--- zsh/modules/ruby.sh
+++ zsh/modules/ruby.sh
@@ -7,4 +7,6 @@
 # ruby version manager
 path_append $HOME/.rvm/bin
 
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
 echo "-> $(basename $0 .sh) is loaded."
