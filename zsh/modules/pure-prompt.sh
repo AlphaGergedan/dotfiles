d65eb91845b2a62094901eec0862dc60947637bd
Atamert Rahma <atamertrahma@gmail.com>
2024-08-01 14:32:24 +0200

    change pure prompts and fix colors

--- zsh/modules/pure-prompt.sh
+++ zsh/modules/pure-prompt.sh
@@ -9,13 +9,21 @@ fpath+=$HOME/.zsh/pure
 autoload -Uz promptinit compinit
 
 # customize prompts
-PURE_PROMPT_SYMBOL='->'
-PURE_PROMPT_VICMD_SYMBOL='<-'
-PURE_GIT_DOWN_ARROW='PULL'
-PURE_GIT_UP_ARROW='PUSH'
-PURE_GIT_STASH_SYMBOL='STASH'
+# ideas: ⬆ ➜ ➤ ➩ ⤳  ⤑  ⤇  ⟿ ⟾   ⇮ ⇝  ⇝  ↧  ↥  ↣  🔃 -> <-
+# ├─
+# ╠═
+PURE_PROMPT_SYMBOL=' ↣'
+PURE_PROMPT_VICMD_SYMBOL=' ↢'
+PURE_GIT_DOWN_ARROW='↧' # PULL
+PURE_GIT_UP_ARROW='↥'
+PURE_GIT_STASH_SYMBOL='📦'
 
 promptinit
 prompt pure
 
-echo "-> $(basename $0 .sh) loaded.."
+# colors
+zstyle ':prompt:pure:git:branch' color 'red'
+zstyle ':prompt:pure:virtualenv' color 'cyan'
+zstyle ':prompt:pure:git:dirty' color 'cyan'
+
+echo " ➜ $(basename $0 .sh) loaded.."
