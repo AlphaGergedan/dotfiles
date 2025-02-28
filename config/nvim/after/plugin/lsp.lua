4231176895ea1d7d8e97610f3b701aaa6a6829a8
Atamert Rahma <atamertrahma@gmail.com>
2025-02-28 19:47:51 +0100

    add signature help on shift+tab

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -5,6 +5,8 @@ lsp_zero.on_attach(function(client, bufnr)
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<S-Tab>", function() vim.lsp.buf.signature_help() end, { noremap = true, silent = true })
+  vim.keymap.set("i", "<S-Tab>", function() vim.lsp.buf.signature_help() end, { noremap = true, silent = true })
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
