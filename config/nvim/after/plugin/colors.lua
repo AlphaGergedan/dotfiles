f9cbdd49d838277d2f182668dd073e7247131372
Atamert Rahma <atamert.rahma@tum.de>
2025-06-20 21:54:35 +0200

    fix dynamic color change

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -1,14 +1,12 @@
 function ColorMyPencils(color, mode)
-	color = color or "gruvbox"
+	color = color or "gruvbox-material"
 
   local current_hour = os.date("*t").hour
-  mode = mode or "light"
   if current_hour >= 16 then
     mode = mode or "dark"
   end
 
-  -- TODO select a light colorscheme if it's early,
-  -- otherwise a dark colorscheme, but how to pick? randomly?
+  mode = mode or "light" -- fallback to light
 
 	vim.cmd.colorscheme(color)
   vim.opt.background = mode
@@ -18,50 +16,8 @@ function ColorMyPencils(color, mode)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   end
-
 end
 
--- light: "edge"
--- dark : "aylin", ""
-
---ColorMyPencils("rose-pine-main", "dark")
---ColorMyPencils("rose-pine-dawn", "dark")
---ColorMyPencils("gruvbox", "light")
---ColorMyPencils("gruvbox", "light")
---ColorMyPencils("kanagawa", "light")
---ColorMyPencils("kanagawa-dragon", "dark")
---ColorMyPencils("gruvbox")
---ColorMyPencils("pink-moon", "dark")
---ColorMyPencils("edge", "dark")
-
---ColorMyPencils("kanagawa", "light")
---ColorMyPencils("kanagawa", "dark")
---ColorMyPencils("kanagawa-dragon", "dark")
-
---ColorMyPencils("gruvbox", "dark")
-ColorMyPencils("gruvbox-material", "dark")
---ColorMyPencils("cyberdream-light", "dark")
-
---ColorMyPencils("kanagawa", "light")
-
-
-
---ColorMyPencils("aylin", "dark")
-
-
-
-
-
-
-
-
-
--- sooooothing
---ColorMyPencils("rose-pine-dawn", "light")
---ColorMyPencils("cyberdream-light", "light")
---ColorMyPencils("gruvbox", "light")
---ColorMyPencils("kanagawa", "light")
---ColorMyPencils("kanagawa", "light")
-
---ColorMyPencils("monokai_pro")
---
+-- dark: 'rose-pine-main', 'aylin', 'kanagawa-dragon', 'gruvbox-material', 'monokai_pro', 'edge', 'pink-moon'
+-- light: 'gruvbox-material', 'kanagawa', 'gruvbox', 'cyberdream-light', 'rose-pine-dawn'
+ColorMyPencils("gruvbox-material")
