4a6d95653245034e8e24001827587c75dd99f7ff
Atamert Rahma <atamert.rahma@tum.de>
2025-06-27 11:47:34 +0200

    fix hours for dynamic coloring

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -2,7 +2,7 @@ function ColorMyPencils(color, mode)
 	color = color or "gruvbox-material"
 
   local current_hour = os.date("*t").hour
-  if current_hour >= 16 then
+  if current_hour >= 16 or current_hour <= 5 then
     mode = mode or "dark"
   end
 
